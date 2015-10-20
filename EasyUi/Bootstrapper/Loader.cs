b900e4ac2fcebd6015c348f39d8294e8ede29680
chrisp9 <chris_powell9@outlook.com>
2015-10-20 21:42:10 +0100

    Initial commit

---
+++ EasyUi/Bootstrapper/Loader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bootstrapper
+{
+    public class Loader
+    {
+        public static void Inject()
+        {
+            MessageBox.Show("Hello");
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                MessageBox.Show("Hello again");
+                Thread.Sleep(5000);
+                MessageBox.Show("Hello again again");
+            });
+        }
+    }
+}
---
+++ EasyUi/Loader/Class1.cs
@@ -0,0 +1,22 @@
+﻿using ManagedInjector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = Process.GetProcessesByName("HardDiskBackup").FirstOrDefault();
+            Injector.Launch(proc.MainWindowHandle, typeof(Bootstrapper.Loader).Assembly.Location, typeof(Bootstrapper.Loader).FullName, "Inject");
+        
+        }
+    }
+}
